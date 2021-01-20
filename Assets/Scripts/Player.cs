1cab597a433ace714ed9010b067c661693cce527
DanP1e <77672369+DanP1e@users.noreply.github.com>
2021-01-20 20:54:20 +0200

    Загрузил проект)
    Это конец.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed;
+    public Rigidbody Rigidbody;
+    void Update()
+    {
+        Rigidbody.velocity = new Vector3();
+        Vector3 movePose = new Vector3(Input.GetAxis("Horizontal"), Rigidbody.velocity.y, Input.GetAxis("Vertical")) * Speed;
+        Rigidbody.velocity = movePose; 
+    }
+}
